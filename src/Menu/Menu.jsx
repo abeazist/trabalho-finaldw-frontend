22dddf9a32cbec9cba33a23cd13b03cc7bf4e63b
beatriz <128046453+abeazist@users.noreply.github.com>
2024-12-03 23:18:12 -0300

    tela de edição e delete pronta
    - qq deu no meu vercel meudeus
    - arrumar o main
    - começar o método de criar aluno, igual da lista

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -4,7 +4,7 @@ export default function Menu() {
       return (
         <div className="min-h-screen bg-white flex">
           {/* Sidebar */}
-          <aside className="w-64 bg-purple-600 text-white flex flex-col justify-between p-6">
+          <aside className="w-64 bg-gradient-to-l from-purple-500 to-purple-600  text-white flex flex-col justify-between p-6">
             {/* Perfil */}
             <div>
               <div className="flex items-center space-x-4">
@@ -13,7 +13,7 @@ export default function Menu() {
                 </div>
                 <div>
                   <h1 className="text-lg font-bold">Bem vindo</h1>
-                  <p className="text-sm text-purple-200">sistema de edição e delete</p>
+                  <p className="text-sm text-purple-200">sistema dos alunos</p>
                 </div>
               </div>
               <nav className="mt-10">
@@ -30,7 +30,7 @@ export default function Menu() {
             </div>
             
             <div>
-              <p className=" flex justify-center w-full bg-purple-500 hover:bg-purple-700 text-white py-2 px-4 rounded-lg">
+              <p className=" flex justify-center w-full bg-purple-700 hover:bg-purple-700 text-white py-2 px-4 rounded-lg">
                 Logout
               </p>
             </div>
@@ -39,7 +39,7 @@ export default function Menu() {
           <main className="flex-1 p-10">
             <header className="flex justify-between items-center mb-8">
               <h1 className="text-2xl font-bold text-purple-600">Lista de Alunos</h1>
-              <button className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-lg">
+              <button className="bg-purple-700 hover:bg-purple-700 text-white px-6 py-2 rounded-lg">
                 Adicionar Estudante
               </button>
             </header>
